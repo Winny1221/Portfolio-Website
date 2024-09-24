87a833e7e3e63d80f74b8f254886df574faefd84
Waleed Kashif <waleed.kashif@builtinsoft.com>
2024-09-24 15:16:52 -0700

    first commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "WALEED KASHIF",
+  profile: '/profile.jpeg',
+  designation: "React Js Developer",
+  description: "My name is WALEED KASHIF. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'rajpootwaleed768@gmail.com',
+  phone: '+923130166184',
+  address: 'Pakistan, Rahim Yar Khan, Adda Gulberg - 64200 ',
+  github: 'https://github.com/Winny1221',
+  facebook: 'https://www.facebook.com/WaleedKashif.Official?mibextid=ZbWKwL',
+  linkedIn: 'https://www.linkedin.com/in/waleed-kashif-1861a4242/',
+  twitter: 'https://twitter.com/said7388',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "waleed12",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
---
+++ utils/data/projects-data.js
@@ -0,0 +1,62 @@
+import ayla from '/public/image/ayla.jpg';
+import crefin from '/public/image/crefin.jpg';
+import realEstate from '/public/image/real-estate.jpg';
+import travel from '/public/image/travel.jpg';
+
+export const projectsData = [
+    {
+        id: 1,
+        name: 'E-commerce website',
+        description: "I have designed an E-commerce website. I have developed a Front-End of this website.",
+        tools: ['React js', 'Type Script', 'Firebase', 'Redux'],
+        role: 'Front-End Developer',
+        code: '',
+        demo: '',
+        image: crefin,
+    },
+    {
+        id: 2,
+        name: 'Admin Panel',
+        description: 'I have designed and developed a Admin Panel for E-commerce website. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, and React Slick. The app supports multiple languages and currencies.',
+        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript"],
+        role: 'Front-End Developer',
+        code: '',
+        demo: '',
+        image: travel,
+    },
+    {
+        id: 3,
+        name: 'Vendor Panel',
+        description: 'I have designed an Vendor Panel for E-commerce website. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, and React Slick.',
+        tools: ['React', 'Bootstrap', 'MUI', 'TailwindCSS', 'Google Maps', 'TypeScript', 'React Slick'],
+        code: '',
+        role: 'Front-End Developer',
+        demo: '',
+        image: realEstate,
+    },
+    // {
+    //     id: 4,
+    //     name: 'Newsroom Management',
+    //     description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
+    //     tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
+    //     code: '',
+    //     demo: '',
+    //     image: ayla,
+    //     role: 'Full Stack Developer',
+    // }
+];
+
+
+// Do not remove any property.
+// Leave it blank instead as shown below
+
+// {
+//     id: 1,
+//     name: '',
+//     description: "",
+//     tools: [],
+//     role: '',
+//     code: '',
+//     demo: '',
+//     image: crefin,
+// },
